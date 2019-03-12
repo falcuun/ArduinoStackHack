e04741761639f750be4b5828ae971cd34995e4ac
falcuun <falcuun@gmail.com>
2019-03-12 19:59:06 +0100

    Added basic declaration

--- ArduinoStackCheat.ino
+++ ArduinoStackCheat.ino
@@ -1,4 +1,13 @@
 #include <Servo.h> // The 'Cheat' will need a servo motor to run
 
-void setup() {}
-void loop() {}
+#define SERVO_PIN 5
+Servo servo;
+
+void setup() {
+    pinMode(SERVO_PIN, OUTPUT); 
+    servo.attach(SERVO_PIN); // Assign the pin that the Servo will be attached to
+}
+
+void loop() {
+    
+}
